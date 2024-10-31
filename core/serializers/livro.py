e937b4f2196dfacc8f0788d443e77a00a6e844a8
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2024-10-30 23:19:24 -0300

    feat: Dois serializadores para Livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -5,3 +5,9 @@ class LivroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'
+        
+class LivroDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
--- core/views/livro.py
+++ core/views/livro.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
 from core.models import Livro
-from core.serializers import LivroSerializer
+from core.serializers import LivroDetailSerializer, LivroSerializer
 
 class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return LivroDetailSerializer
+        return LivroSerializer
