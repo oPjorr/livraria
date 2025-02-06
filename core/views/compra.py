c4a89f7007ff563493271a2dac62b85c2cab8654
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2025-02-06 09:47:40 -0300

    feat: Gerando um relatório de vendas do mês

--- core/views/compra.py
+++ core/views/compra.py
@@ -1,8 +1,13 @@
+from django.db import transaction
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Compra
 from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.filters import OrderingFilter, SearchFilter
 from core.serializers import CompraSerializer, CriarEditarCompraSerializer
+from datetime import timezone
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -54,3 +59,22 @@ class CompraViewSet(ModelViewSet):
             compra.save()
 
         return Response(status=status.HTTP_200_OK, data={"status": "Compra finalizada"})
+    
+    @action(detail=False, methods=["get"])
+    def relatorio_vendas_mes(self, request):
+        agora = timezone.now()
+        inicio_mes = agora.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+        compras = Compra.objects.filter(status=Compra.StatusCompra.FINALIZADO, data__gte=inicio_mes)
+
+        total_vendas = sum(compra.total for compra in compras)
+        quantidade_vendas = compras.count()
+
+        return Response(
+            {
+                "status": "Relatório de vendas deste mês",
+                "total_vendas": total_vendas,
+                "quantidade_vendas": quantidade_vendas,
+            },
+            status=status.HTTP_200_OK,
+        )
