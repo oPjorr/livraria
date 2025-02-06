91afa776aeb1bc889c5d544cd2b0bf64474c9980
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2025-02-06 10:48:01 -0300

    feat: Adicionando busca textual

--- core/views/livro.py
+++ core/views/livro.py
@@ -11,8 +11,9 @@ from core.serializers import LivroDetailSerializer, LivroSerializer, LivroListSe
 class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ["categoria__descricao", "editora__nome"]
+    search_fields = ["titulo"]
 
     def get_serializer_class(self):
         if self.action == "list":
