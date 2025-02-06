e904e143140efeb17415c8df9424da95aa5798f2
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2025-02-06 09:43:55 -0300

    feat: Finalizando a compra e atualizando a quantidade de itens em estoque

--- core/views/compra.py
+++ core/views/compra.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Compra
+from rest_framework.decorators import action
 from core.serializers import CompraSerializer, CriarEditarCompraSerializer
 from rest_framework.permissions import IsAuthenticated
 
@@ -19,8 +20,37 @@ class CompraViewSet(ModelViewSet):
         return Compra.objects.filter(usuario=usuario)
 
     def get_serializer_class(self):
-        if self.action == "list":
-            return CompraListSerializer
         if self.action in ("create", "update"):
-            return CompraCreateUpdateSerializer
+            return CriarEditarCompraSerializer
         return CompraSerializer
+    
+    @action(detail=True, methods=["post"])
+    def finalizar(self, request, pk=None):
+        compra = self.get_object()
+
+        if compra.status != Compra.StatusCompra.CARRINHO:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={"status": "Compra já finalizada"},
+            )
+
+        with transaction.atomic():
+            for item in compra.itens.all():
+
+                if item.quantidade > item.livro.quantidade:
+                    return Response(
+                        status=status.HTTP_400_BAD_REQUEST,
+                        data={
+                            "status": "Quantidade insuficiente",
+                            "livro": item.livro.titulo,
+                            "quantidade_disponivel": item.livro.quantidade,
+                        },
+                    )
+
+                item.livro.quantidade -= item.quantidade
+                item.livro.save()
+
+            compra.status = Compra.StatusCompra.FINALIZADO
+            compra.save()
+
+        return Response(status=status.HTTP_200_OK, data={"status": "Compra finalizada"})
